95896d748202b71b82e985d92db5f76e6efdc9c9
Levan Gelashvili <levan.gelashvili9@gmail.com>
2024-11-30 20:38:50 +0400

    feat: spinner

--- features/tasks/components/active-tasks-list.tsx
+++ features/tasks/components/active-tasks-list.tsx
@@ -5,6 +5,7 @@ import { useQueryState } from "nuqs";
 import { TaskStatus, useTasks } from "@/features/tasks/api";
 
 import { TaskCard } from "@/features/tasks/components";
+import { Spinner } from "@/components/ui/spinner";
 
 export const ActiveTasksList = () => {
   const [searchParams] = useQueryState("search", {
@@ -16,7 +17,9 @@ export const ActiveTasksList = () => {
     status: TaskStatus.Pending,
   });
 
-  if (status === "pending") return <p>Loading tasks...</p>;
+  if (status === "pending")
+    return <Spinner className="absolute left-1/2 top-1/2" />;
+
   if (status === "error") return <p>Error loading tasks</p>;
 
   return (
