a2f907aa41d4b9cc528e5735c8080daa4c4a0660
Levan Gelashvili <levan.gelashvili9@gmail.com>
2024-11-30 20:10:07 +0400

    chore: add garbage collector

--- components/shared/query-provider.tsx
+++ components/shared/query-provider.tsx
@@ -10,9 +10,8 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
         staleTime: 5 * 60 * 1000,
+        gcTime: 2 * 60 * 1000,
       },
     },
   });
